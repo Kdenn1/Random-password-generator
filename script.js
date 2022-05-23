b13678b8a9d43981ec44974ec7ae30471c4c566f
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-05-23 16:55:47 -0700

    got it to where console logs random numbers, uppercase and lowercase letters, and symbols

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // Assignment code here
 
+//main elements
+
+
+ 
+//generate password function
+
+
+
+//random generated character functions 
+
+
+function getRandomLower() {
+return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  console.log(getRandomUpper());
+
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    }
+    console.log(getRandomNumber());
+
+    function getRandomSymbol() {
+      const symbols = '!@?&%$#';
+      return symbols [Math.floor(Math.random() * symbols.length)];
+      }
+      console.log(getRandomSymbol());
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +46,15 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
+ 
+
+
+
+
+
+
+
+
+
+
